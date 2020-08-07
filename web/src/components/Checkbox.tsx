378cea2c560b5e09858a811f9f3d1e106a27ff37
Paul Schwörer <schwoerer@sipgate.de>
2020-08-07 12:06:23 +0200

    remove styled-components in Checkbox

--- web/src/components/Checkbox.tsx
+++ web/src/components/Checkbox.tsx
@@ -1,21 +1,21 @@
 import React, { ReactElement, ChangeEvent } from "react";
 import Icon from "./Icon/Icon";
 import { faSquare, faCheckSquare } from "@fortawesome/free-solid-svg-icons";
-import styled from "styled-components";
 
 interface Props {
   checked: boolean;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const StyledInput = styled.input`
-  display: none;
-`;
-
 const Checkbox = ({ checked, onChange }: Props): ReactElement => {
   return (
     <>
-      <StyledInput type="checkbox" checked={checked} onChange={onChange} />
+      <input
+        style={{ display: "none" }}
+        type="checkbox"
+        checked={checked}
+        onChange={onChange}
+      />
       {!checked && <Icon icon={faSquare} />}
       {checked && <Icon icon={faCheckSquare} />}
     </>

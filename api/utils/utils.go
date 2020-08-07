75fa8578d4d1329a48226569109a8f5e2b8e639d
Paul Schwörer <schwoerer@sipgate.de>
2020-08-07 17:43:15 +0200

    wip: use query endpoint for instances list

--- api/utils/utils.go
+++ api/utils/utils.go
@@ -1,5 +1,7 @@
 package utils
 
+import "encoding/json"
+
 func IsInSlice(needle string, list []string) bool {
 	for _, entry := range list {
 		if entry == needle {
@@ -7,4 +9,14 @@ func IsInSlice(needle string, list []string) bool {
 		}
 	}
 	return false
+}
+
+func ConvertMapToStruct(source map[string]interface{}, target interface{}) error {
+	data, err := json.Marshal(source)
+
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, target)
 }

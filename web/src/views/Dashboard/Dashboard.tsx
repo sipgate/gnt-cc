72811ff46770611d254e50275eb621415a7587c0
Paul Schwörer <hello@paulschwoerer.de>
2020-09-18 16:12:13 +0200

    WEB: add statistics to dashboard

--- web/src/views/Dashboard/Dashboard.tsx
+++ web/src/views/Dashboard/Dashboard.tsx
@@ -4,39 +4,61 @@ import Card from "../../components/cards/Card/Card";
 import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
 import AuthContext from "../../api/AuthContext";
 import Hero from "../../components/Hero/Hero";
+import { useApi } from "../../api";
+import { useClusterName } from "../../helpers/hooks";
+import LoadingIndicator from "../../components/LoadingIndicator/LoadingIndicator";
+import { convertMBToGB } from "../../helpers";
 
-const dummyStats = [
-  {
-    title: "Nodes",
-    value: "12",
-  },
-  {
-    title: "Instances",
-    value: "342",
-  },
-  {
-    title: "CPU Cores",
-    value: "77",
-  },
-  {
-    title: "Disk Space",
-    value: "5 TB",
-  },
-  {
-    title: "Memory",
-    value: "256 GB",
-  },
-];
+interface StatisticElement {
+  count: number;
+  memoryTotal: number;
+  cpuCount: number;
+}
+interface StatisticsResponse {
+  instances: StatisticElement;
+  nodes: StatisticElement;
+}
 
 function Dashboard(): ReactElement {
+  const clusterName = useClusterName();
   const authContext = useContext(AuthContext);
+  const [{ data, isLoading, error }] = useApi<StatisticsResponse>(
+    `clusters/${clusterName}/statistics`
+  );
+
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
+
+  if (!data) {
+    return <div>Failed to load: {error}</div>;
+  }
+
+  const stats = [
+    {
+      title: "Nodes",
+      value: data.nodes.count,
+    },
+    {
+      title: "Instances",
+      value: data.instances.count,
+    },
+    {
+      title: "Node CPU Cores",
+      value: data.nodes.cpuCount,
+    },
+    {
+      title: "Total Node Memory",
+      value: `${convertMBToGB(data.nodes.memoryTotal)} GB`,
+    },
+  ];
 
   return (
     <>
       <Hero title={`Welcome back, ${authContext.username}!`} />
       <ContentWrapper>
         <div className={styles.statistics}>
-          {dummyStats.map((stat) => (
+          {stats.map((stat) => (
             <Card key={stat.title} title={stat.title}>
               <div className={styles.stat}>
                 <span>{stat.value}</span>

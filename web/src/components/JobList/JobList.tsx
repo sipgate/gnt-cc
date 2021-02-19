dc2419071b2de4b99d744e2d233ac1d07eca9b3b
Julian Geywitz <geywitz@sipgate.de>
2021-02-19 14:10:33 +0100

    web: styling for job table

--- web/src/components/JobList/JobList.tsx
+++ web/src/components/JobList/JobList.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import styles from "./JobList.module.scss";
 import { GntJob } from "../../api/models";
+import Badge, { BadgeStatus } from "../Badge/Badge";
 import DataTable, { IDataTableColumn } from "react-data-table-component";
 
 const columns: IDataTableColumn<GntJob>[] = [
@@ -8,11 +9,17 @@ const columns: IDataTableColumn<GntJob>[] = [
     name: "ID",
     sortable: true,
     selector: (row) => row.id,
+    width: "120px",
   },
   {
     name: "Status",
     sortable: true,
     selector: (row) => row.status,
+    width: "120px",
+    cell: (row) => {
+      const status = getBadgeStatusFromJobStatus(row.status);
+      return <Badge status={status}>{row.status}</Badge>;
+    },
   },
   {
     name: "Summary",
@@ -23,16 +30,42 @@ const columns: IDataTableColumn<GntJob>[] = [
     name: "Start",
     sortable: true,
     selector: (row) => row.startedAt,
+    format: (row) => {
+      return row.startedAt < 0 ? "-" : row.startedAt;
+    },
+    width: "120px",
   },
   {
-    name: "End",
-    sortable: true,
+    name: "Duration",
+    sortable: false,
+    width: "120px",
     selector: (row) => row.endedAt,
+    format: (row) => {
+      if (row.startedAt < 0 || row.endedAt < 0) {
+        return "-";
+      }
+
+      return row.endedAt - row.startedAt;
+    },
   },
 ];
 
 interface Props {
   jobs: GntJob[];
+}
+
+function getBadgeStatusFromJobStatus(status: string) {
+  if (status === "success") {
+    return BadgeStatus.SUCCESS;
+  }
+  if (["queued", "waiting", "canceling"].includes(status)) {
+    return BadgeStatus.WARNING;
+  }
+  if (status === "error") {
+    return BadgeStatus.FAILURE;
+  }
+
+  return undefined;
 }
 
 function JobList({ jobs }: Props): ReactElement {
@@ -41,10 +74,12 @@ function JobList({ jobs }: Props): ReactElement {
       <DataTable<GntJob>
         columns={columns}
         data={jobs}
-        keyField="name"
+        keyField="id"
         pagination
         paginationPerPage={20}
         noHeader
+        defaultSortAsc={false}
+        defaultSortField="id"
       />
     </div>
   );

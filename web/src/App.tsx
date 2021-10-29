f7430ccf361ba08a1c8af2f46a33064e2f619382
Julian Geywitz <geywitz@sipgate.de>
2021-10-29 14:04:08 +0200

    fix(web): use correct theme on all pages
    Co-authored-by: Paul Schwörer <schwoerer@sipgate.de>

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,25 @@
-import React, { useContext, useState, useEffect, ReactElement } from "react";
+import React, { ReactElement, useContext, useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
-  Switch,
+  Redirect,
   Route,
   RouteProps,
-  Redirect,
+  Switch,
 } from "react-router-dom";
-import Login from "./views/Login/Login";
 import AuthContext from "./api/AuthContext";
-import ClusterWrapper from "./views/ClusterWrapper";
 import JobWatchContext from "./contexts/JobWatchContext";
+import ThemeContext from "./contexts/ThemeContext";
+import ClusterWrapper from "./views/ClusterWrapper";
+import Login from "./views/Login/Login";
 
 const STORAGE_TOKEN_KEY = "gnt-cc-token";
+const LIGHT_CLASS = "light";
+const DARK_THEME_ID = "theme-dark";
+
+const savedThemeState = localStorage.getItem(DARK_THEME_ID);
+const initialState = savedThemeState
+  ? (JSON.parse(savedThemeState) as boolean)
+  : false;
 
 export function AuthenticatedRoute(props: RouteProps): ReactElement {
   const authContext = useContext(AuthContext);
@@ -41,6 +49,29 @@ function App(): ReactElement {
   const storedAuthToken = localStorage.getItem(STORAGE_TOKEN_KEY);
   const [authToken, setAuthToken] = useState(storedAuthToken);
   const [trackedJobs, setTrackedJobs] = useState<number[]>([]);
+  const [isDark, setIsDark] = useState(initialState);
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.remove(LIGHT_CLASS);
+    } else {
+      document.documentElement.classList.add(LIGHT_CLASS);
+    }
+  }, [isDark]);
+
+  useEffect(() => {
+    const setPreferredMode = (prefersDark: boolean) => {
+      if (savedThemeState === null) {
+        setIsDark(prefersDark);
+      }
+    };
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setPreferredMode(mediaQuery.matches);
+    mediaQuery.addListener(({ matches: prefersDark }) =>
+      setPreferredMode(prefersDark)
+    );
+  }, []);
 
   useEffect(() => {
     if (authToken) {
@@ -50,46 +81,62 @@ function App(): ReactElement {
     }
   }, [authToken]);
 
+  function setIsDarkPersistent(value: boolean) {
+    setIsDark(value);
+    localStorage.setItem(DARK_THEME_ID, JSON.stringify(value));
+  }
+
+  function toggleTheme() {
+    setIsDarkPersistent(!isDark);
+  }
+
   const tokenPayload = parseJwtPayload(authToken);
 
   return (
     <div className="App">
-      <AuthContext.Provider
+      <ThemeContext.Provider
         value={{
-          setToken: setAuthToken,
-          username: tokenPayload ? tokenPayload.id : null,
-          authToken,
+          isDark,
+          toggleTheme,
         }}
       >
-        <JobWatchContext.Provider
+        <AuthContext.Provider
           value={{
-            trackJob(jobID) {
-              if (trackedJobs.includes(jobID)) {
-                return;
-              }
-
-              setTrackedJobs([...trackedJobs, jobID]);
-            },
-            untrackJob(jobID) {
-              setTrackedJobs(trackedJobs.filter((id) => id !== jobID));
-            },
-            trackedJobs,
+            setToken: setAuthToken,
+            username: tokenPayload ? tokenPayload.id : null,
+            authToken,
           }}
         >
-          <Router>
-            <Switch>
-              <Route exact path="/login" component={Login} />
+          <JobWatchContext.Provider
+            value={{
+              trackJob(jobID) {
+                if (trackedJobs.includes(jobID)) {
+                  return;
+                }
 
-              <AuthenticatedRoute
-                path="/:clusterName?"
-                component={ClusterWrapper}
-              />
+                setTrackedJobs([...trackedJobs, jobID]);
+              },
+              untrackJob(jobID) {
+                setTrackedJobs(trackedJobs.filter((id) => id !== jobID));
+              },
+              trackedJobs,
+            }}
+          >
+            <Router>
+              <Switch>
+                <Route exact path="/login" component={Login} />
 
-              <Route render={() => <span>404 Not found</span>}></Route>
-            </Switch>
-          </Router>
-        </JobWatchContext.Provider>
-      </AuthContext.Provider>
+                <AuthenticatedRoute
+                  path="/:clusterName?"
+                  component={ClusterWrapper}
+                />
+
+                <Route render={() => <span>404 Not found</span>}></Route>
+              </Switch>
+            </Router>
+          </JobWatchContext.Provider>
+        </AuthContext.Provider>
+      </ThemeContext.Provider>
     </div>
   );
 }

cb4f0dbe84f530a276f259e7fdf807a86e526abc
Paul Schwörer <schwoerer@sipgate.de>
2021-11-26 11:11:32 +0100

    refactor(web): move auth and job watch logic into providers

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useContext, useEffect, useState } from "react";
+import React, { ReactElement, useContext } from "react";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -7,12 +7,11 @@ import {
   Switch,
 } from "react-router-dom";
 import AuthContext from "./api/AuthContext";
-import JobWatchContext from "./contexts/JobWatchContext";
+import AuthProvider from "./providers/AuthProvider";
+import JobWatchProvider from "./providers/JobWatchProvider";
 import ThemeProvider from "./providers/ThemeProvider";
 import ClusterWrapper from "./views/ClusterWrapper";
 import Login from "./views/Login/Login";
-
-const STORAGE_TOKEN_KEY = "gnt-cc-token";
 
 export function AuthenticatedRoute(props: RouteProps): ReactElement {
   const authContext = useContext(AuthContext);
@@ -24,60 +23,12 @@ export function AuthenticatedRoute(props: RouteProps): ReactElement {
   );
 }
 
-interface JwtPayload {
-  id: string;
-  exp: number;
-  orig_iat: number;
-}
-
-const parseJwtPayload = (token: string | null): JwtPayload | null => {
-  if (!token) {
-    return null;
-  }
-
-  return JSON.parse(atob(token.split(".")[1]));
-};
-
 function App(): ReactElement {
-  const storedAuthToken = localStorage.getItem(STORAGE_TOKEN_KEY);
-  const [authToken, setAuthToken] = useState(storedAuthToken);
-  const [trackedJobs, setTrackedJobs] = useState<number[]>([]);
-
-  useEffect(() => {
-    if (authToken) {
-      localStorage.setItem(STORAGE_TOKEN_KEY, authToken);
-    } else {
-      localStorage.removeItem(STORAGE_TOKEN_KEY);
-    }
-  }, [authToken]);
-
-  const tokenPayload = parseJwtPayload(authToken);
-
   return (
     <div className="App">
       <ThemeProvider>
-        <AuthContext.Provider
-          value={{
-            setToken: setAuthToken,
-            username: tokenPayload ? tokenPayload.id : null,
-            authToken,
-          }}
-        >
-          <JobWatchContext.Provider
-            value={{
-              trackJob(jobID) {
-                if (trackedJobs.includes(jobID)) {
-                  return;
-                }
-
-                setTrackedJobs([...trackedJobs, jobID]);
-              },
-              untrackJob(jobID) {
-                setTrackedJobs(trackedJobs.filter((id) => id !== jobID));
-              },
-              trackedJobs,
-            }}
-          >
+        <AuthProvider>
+          <JobWatchProvider>
             <Router>
               <Switch>
                 <Route exact path="/login" component={Login} />
@@ -90,8 +41,8 @@ function App(): ReactElement {
                 <Route render={() => <span>404 Not found</span>}></Route>
               </Switch>
             </Router>
-          </JobWatchContext.Provider>
-        </AuthContext.Provider>
+          </JobWatchProvider>
+        </AuthProvider>
       </ThemeProvider>
     </div>
   );

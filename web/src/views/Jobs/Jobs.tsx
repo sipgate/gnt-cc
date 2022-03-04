117a623926c9260139da6fabd3a7f7b5782ffdb1
Paul Schwörer <schwoerer@sipgate.de>
2022-03-04 13:48:06 +0100

    fix: remove auto refreshing of jobs list
    closes #57
    
    Co-authored-by: Rudolph Bott <bott@sipgate.de>

--- web/src/views/Jobs/Jobs.tsx
+++ web/src/views/Jobs/Jobs.tsx
@@ -1,6 +1,5 @@
-import { faCircleNotch, faRedoAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { ReactElement, useEffect } from "react";
+import { faRedoAlt } from "@fortawesome/free-solid-svg-icons";
+import React, { ReactElement } from "react";
 import { useApi } from "../../api";
 import { GntJob } from "../../api/models";
 import ApiDataRenderer from "../../components/ApiDataRenderer/ApiDataRenderer";
@@ -8,9 +7,6 @@ import Button from "../../components/Button/Button";
 import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
 import JobList from "../../components/JobList/JobList";
 import { useClusterName } from "../../helpers/hooks";
-import styles from "./Jobs.module.scss";
-
-const REFRESH_INTERVAL = 15000;
 
 interface JobResponse {
   jobs: GntJob[];
@@ -22,26 +18,9 @@ const Jobs = (): ReactElement => {
     `clusters/${clusterName}/jobs`
   );
 
-  useEffect(() => {
-    const intervalID = setInterval(reload, REFRESH_INTERVAL);
-    return () => clearInterval(intervalID);
-  }, [clusterName]);
-
   return (
     <ContentWrapper>
-      <div className={styles.refreshControl}>
-        <Button label="Refresh" icon={faRedoAlt} onClick={reload}></Button>
-        {apiProps.isLoading && (
-          <span>
-            Refreshing
-            <FontAwesomeIcon
-              className={styles.refreshIcon}
-              spin
-              icon={faCircleNotch}
-            />
-          </span>
-        )}
-      </div>
+      <Button label="Refresh" icon={faRedoAlt} onClick={reload}></Button>
       <ApiDataRenderer
         {...apiProps}
         render={({ jobs }) => <JobList jobs={jobs} />}
